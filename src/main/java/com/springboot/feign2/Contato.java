5c3715632584eb3fab18e32678263ac82eb3fd94
Andrew Malandrin <del_malandrin@hotmail.com>
2022-05-31 14:29:48 -0300

    First Commit

---
+++ src/main/java/com/springboot/feign2/Contato.java
@@ -0,0 +1,14 @@
+package com.springboot.feign2;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Contato {
+
+    private Long id;
+    private String nome;
+    private String telefone;
+
+}
---
+++ src/test/java/com/springboot/feign2/Feign2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.feign2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Feign2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
